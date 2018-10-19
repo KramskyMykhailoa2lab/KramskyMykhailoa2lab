72f38dde47cef34b0393561b3c2611656056930a
KramskyMykhailoa2lab <wows623@mail.ru>
2018-10-19 12:05:52 +0300

    first commit

---
+++ lesson_1.Exp_4/JS/main.js
@@ -0,0 +1,8 @@
+document.getElementById('buttonClasses').onclick = function() {
+    document.getElementById('TableCall').style.display='none';
+    document.getElementById('TableClasses').style.display='block';
+}
+document.getElementById('buttonCall').onclick = function() {
+    document.getElementById('TableClasses').style.display='none';
+    document.getElementById('TableCall').style.display='block';
+}
